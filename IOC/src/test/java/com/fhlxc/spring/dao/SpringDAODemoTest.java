a80c1d6686bbefef1e28ca0f42b4f8fa0361b6dd
Xingchao Long <z15881614642@163.com>
2020-01-31 16:52:38 +0800

    2020年1月31日16:52:35

--- IOC/src/test/java/com/fhlxc/spring/dao/SpringDAODemoTest.java
+++ IOC/src/test/java/com/fhlxc/spring/dao/SpringDAODemoTest.java
@@ -1,5 +1,10 @@
 package com.fhlxc.spring.dao;
 
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
 * @author Xingchao Long
 * @date 2020年1月30日 下午5:30:36
@@ -9,8 +14,16 @@ package com.fhlxc.spring.dao;
 
 public class SpringDAODemoTest {
 
+    @SuppressWarnings("resource")
     public static void main(String[] args) {
-
+        ApplicationContext context = new ClassPathXmlApplicationContext("springDAODemo.xml");
+        IUserDAO userDAO = (IUserDAO) context.getBean("userDao");
+        User user = new User("JdbcDaoSupportTest", 26, 333.33);
+        userDAO.addUser(user);
+        List<User> list = userDAO.queryAllUser();
+        for (User u: list) {
+            System.out.println(u.toString());
+        }
     }
 
 }
