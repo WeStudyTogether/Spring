024bd6c88956710a615f77bca68716f7b70062e3
fhlxc <z15881614642@163.com>
2020-03-14 21:34:03 +0800

    2020年03月14日
    打包

--- HelloWorld/src/main/java/com/fhlxc/demo/HelloWorldApplication.java
+++ HelloWorld/src/main/java/com/fhlxc/demo/HelloWorldApplication.java
@@ -2,14 +2,21 @@ package com.fhlxc.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan(basePackages = {"com.fhlxc.demo", "com.fhlxc.services", "com.fhlxc.config"})
 @SpringBootApplication
 @EnableCaching
-public class HelloWorldApplication {
+public class HelloWorldApplication extends SpringBootServletInitializer {
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(HelloWorldApplication.class);
+    }
+    
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
 	}
